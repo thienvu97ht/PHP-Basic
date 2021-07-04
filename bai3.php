104bdb5a8d08f230b5b7fd476f7ddf53eecbd234
thienvu97ht <thien.vu97ht123@gmail.com>
2021-07-04 18:17:40 +0700

    Bai 3

---
+++ bai3.php
@@ -0,0 +1,46 @@
+<?php
+$arr = [1, 2, 3, 4, 5, 6];
+
+// for ($i = 0; $i < count($arr); $i++) {
+//     echo $arr[$i];
+//     echo "<br/>";
+// };
+
+// $i = 0;
+// while ($i < count($arr)) {
+//     echo $arr[$i];
+//     echo "<br/>";
+//     $i++;
+// };
+
+// $i = 0;
+// do {
+//     echo $arr[$i];
+//     echo "<br/>";
+//     $i++;
+// } while ($i < count($arr));
+
+// foreach ($arr as $value) {
+//     echo $value;
+// };
+
+$arr = [
+    "Fullname" => "Dương Đình Thiện Vũ",
+    "Gender" => "Nam",
+    "Address" => "HÀ TĨNH"
+];
+
+// foreach ($arr as $key => $value) {
+//     echo $key . ": " . $value;
+//     echo "<br/>";
+// };
+
+function showMenu($showInfo)
+{
+    foreach ($showInfo as $key => $value) {
+        echo $key . ": " . $value;
+        echo "<br/>";
+    };
+};
+
+showMenu($arr);
