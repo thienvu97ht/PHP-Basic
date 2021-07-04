67ec3071d3c01cf23e5926f350057d7582973fd1
thienvu97ht <thien.vu97ht123@gmail.com>
2021-07-04 17:32:45 +0700

    Bai 2

---
+++ bai2.php
@@ -0,0 +1,34 @@
+<?php
+
+// const URL_BASE = "https://vudeptrai.xyz";
+// define("WELCOME", "Hello World");
+
+// $x = 5;
+
+
+// $x = "Xin Chào";
+// $str = "Dương Đình Thiện Vũ";
+
+// echo URL_BASE;
+// echo "<br/>";
+// echo WELCOME;
+// echo "<br/>";
+
+// echo $x . " " . $str;
+
+$arr = array(1, 5, 7, 8, 10);
+
+$arr2 = [
+    [
+        "id" => "1",
+        "name" => "Balo Hạt Nhân Degrey",
+        "price" => 420000,
+        "images" => [
+            [
+                "url" => "https://product.hstatic.net/1000281824/product/6494b116-a95f-42e1-bd99-33ae0fef78a3_c36ead4edfde4943b98b4ad16eb74463_grande.jpeg"
+            ]
+        ]
+    ],
+];
+
+echo $arr2[0]['name'];
